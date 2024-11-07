7895e96f924a12972ff906c3299c5c675e811e09
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2024-11-07 13:24:31 -0500

    okay so I started creating the graph and have that working I think, I need to delete the words and sink from the graph each time we have a new word, we will also do all the logic in that same while loop when we run through the words file

--- worddice.cpp
+++ worddice.cpp
@@ -1,48 +1,151 @@
 #include <iostream>
 #include <vector>
-
+#include <fstream>
 
 using namespace std;
 
-enum node_type{src, sink, dice, word};
+enum node_type { src, sink, dice, word };
 
 class Edge {
+public:
     class Node *to;
     class Node *from;
     Edge *reverse;
-    int original = 1;
-    int residual = 0;
+    int original;
+    int residual;
     int index;
 };
 
 class Node {
+public:
     int id;
     node_type type;
     vector<bool> letters;
-    Node();
     int visited;
     vector<Edge*> adj;
-    Edge *backEdge
+    Edge *backEdge;
+
+    // Constructor
+    Node() 
+        :
+          letters(26, false),  // 26 spots for each letter of the alphabet
+          visited(0)  {}
 };
 
 class Graph {
+public:
     vector<Node*> nodes;
+    vector<int> spellingIds;
+    int minNodes=0;
+
     int BFS();
     int canISpell();
-    vector<int> spellingIds;
-    void deleteHaldGraph();\
-    int minNodes;
-}
+    void deleteHalfGraph();
+};
 
 int main(int argc, char *argv[]) {
+
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <diceFile> <wordFile>" << endl;
+        return 1;
+    }
+
+    string diceFile = argv[1];
+    string wordFile = argv[2];
+    int nodeIndex=0;
+
+    cout << "dice: " << diceFile << endl;
+    cout << "words: " << wordFile << endl;
+
+    Graph ourGraph;
+    Node *source = new Node;
     
-    string diceFile, wordFile;
+    source->id = 0;
+    source->type = src;
 
-    diceFile = argv[1];
-    wordFile = argv[2];
+    ourGraph.nodes.push_back(source);
+    ourGraph.minNodes++;
 
-    cout << "dice " <<  diceFile << endl;
-    cout << "words " << wordFile << endl;
+    ifstream fin(diceFile);
+
+    if(!fin.is_open()){
+		cerr << "Could not open: " << diceFile << endl;
+	}
+
+    string line;
+    while (getline(fin, line)) {
+        nodeIndex++;
+        cout << line << endl;
+        Node *ourDice = new Node;;
+        ourDice->id = nodeIndex;
+        ourDice->type = dice;
+        for(int i=0; i < line.length(); i++){
+            char letter = line[i];
+            int diff = letter - 'A';
+            ourDice->letters[diff]=true;
+        }
+        Edge *fromSource = new Edge;
+        fromSource->from = source;
+        fromSource->to = ourDice;
+        fromSource->original = 1;
+        fromSource->residual = 0;
+        ourDice->backEdge = fromSource; //is this going the right direction? Do we need this yet? do we want this yet?
+        source->adj.push_back(fromSource);
+        ourGraph.nodes.push_back(ourDice);
+        ourGraph.minNodes++;
+    }
+
+    cout << ourGraph.nodes.size() << endl;
+    cout << "count of nodes" << ourGraph.minNodes << endl;
+
+    fin.close();
+
+    fin.open(wordFile);
+
+    if(!fin.is_open()){
+		cerr << "Could not open: " << wordFile << endl;
+	}
+
+    while(getline(fin, line)){
+        Node *ourSink = new Node();
+        ourSink->type = sink;
+        for(int i = 0; i < line.length(); i++){
+            nodeIndex++;
+            cout << line[i] << endl;
+            Node *aLetter = new Node();
+            aLetter->id = nodeIndex;
+            aLetter->type = word;
+            char letter = line[i];
+            int diff = letter - 'A';
+            aLetter->letters[diff]=true;
+
+            Edge *toSink = new Edge;
+            toSink->from = aLetter;
+            toSink->to = ourSink;
+            toSink->original = 1;
+            toSink->residual = 0;
+
+            aLetter->adj.push_back(toSink);
+
+            ourGraph.nodes.push_back(aLetter);
+        }
+        ourSink->id = nodeIndex + 1;
+        ourGraph.nodes.push_back(ourSink);
+
+        //Now I am gonna print out the graph to make sure it looks right
+
+        for(int i = 0; i < ourGraph.nodes.size(); i++){
+            cout << "Node " << i << " type: " << ourGraph.nodes[i]->type << " Edges to " ;
+            for(int j = 0; j < ourGraph.nodes[i]->adj.size(); j++){
+                cout << ourGraph.nodes[i]->adj[j]->to->id << " " ;
+            }
+            cout << endl;
+        }
+
+        //need to delete now the letters and the sink at the end of this loop every time
+        //we can use the function in the class if we want but idk it we want to; we might want to use at the 
+        //end to delete all of it but can play around with it and see
+    }
 
     return 0;
 }
