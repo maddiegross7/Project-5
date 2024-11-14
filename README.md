1c8ab025cc1ef4aed9a1da2367f428efc507ee8a
Madelyn Gross <mgross10@hydra7.eecs.utk.edu>
2024-11-13 21:24:25 -0500

    okay I think I am done but I am failing all the gradescripts and no one knows why

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Project-5
+So I was the only one to work on this project so I did this by myself
+I did take a lot of direction from the videos that crumpton posted they were very
+helpful
+
+I am pretty sure that my time complexity would be categorized as O(n^2) since there are
+various cases of for loops within for loops which can take longer, I used nodes and edges 
+to accomplish this and created a connected graph but that meant iterating through a few 
+lists here and there
--- worddice.cpp
+++ worddice.cpp
@@ -1,3 +1,6 @@
+//Maddie Gross
+//Lab 5: Worddice Lab
+//Uses Endmonds-Karp algorithm to find if a word can be spelled using a specified number of dice
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -7,6 +10,7 @@ using namespace std;
 
 enum node_type { src, sink, dice, word };
 
+//what connects the nodes
 class Edge {
 public:
     class Node *to;
@@ -17,6 +21,7 @@ public:
     int index;
 };
 
+//sink, src, dice, letters in each word
 class Node {
     public:
         int id;
@@ -35,6 +40,7 @@ Node::Node(){
     id = NULL;
 }
 
+//holds all of them
 class Graph {
     public:
         vector<Node*> nodes;
@@ -43,120 +49,154 @@ class Graph {
         int BFS();
         int canISpell();
         void deleteHalfGraph();
+        void cleanupGraph();
 };
 
-//okay gotta make this work all the way and not just till 
-//we find a route
-int Graph::BFS(){
+//Uses breadth first search to explore various routes in order to get to the sink
+int Graph::BFS() {
     queue<Node*> q;
     Node *ourSource = nodes[0];
-    //cout << "we in the BFS" << endl;
-    for(int i = 0; i < nodes.size(); i++){
-        nodes[i]->visited = 0;
-        nodes[i]->backEdge = nullptr;
-    }
-    ourSource->visited = 1;
 
+    for (Node* node : nodes) {
+        node->visited = 0;
+        node->backEdge = nullptr;
+    }
+
+    ourSource->visited = 1;
     q.push(ourSource);
 
-    while (!q.empty()){
+    while (!q.empty()) {
         Node *current = q.front();
         q.pop();
 
-        for(int i = 0; i < current->adj.size(); i++){
-            Node *next = current->adj[i]->to;
+        for (Edge* edge : current->adj) {
+            Node *next = edge->to;
+            if (edge->from == current) {
+                if (!next->visited && (edge->original - edge->residual) > 0) {
+                    next->visited = 1;
+                    next->backEdge = edge;
+                    q.push(next);
 
-            //let it work if someday the flow is not just 1
-            if(!next->visited && (current->adj[i]->original - current->adj[i]->residual) > 0){
-                next->visited = 1;
-                next->backEdge = current->adj[i];
-                
-                q.push(next);
-                
-
-                if(next->type == sink){
-                    return 1;
+                    if (next->type == sink) {
+                        return 1;  
+                    }
                 }
-            }//else if the reverse has some kind of original flow then add that to the q maybe?
+            } else {
+                //this is to explore reverse edges if we may need to change dice from orginal flow
+                Edge* reverseEdge = edge; 
+                if (reverseEdge->reverse) {
+                    Node *previous = reverseEdge->from;
 
+                    if (!previous->visited && reverseEdge->residual > 0) {
+                        previous->visited = 1;
+                        previous->backEdge = reverseEdge; 
+                        q.push(previous);
+
+                        if (previous->type == sink) {
+                            return 1;  
+                        }
+                    }
+                }
+            }
         }
     }
-    return 0;
+
+    return 0; 
 }
 
+//calls BFS multiple times and adjusts the flows of the paths that are found each iteration
 int Graph::canISpell(){
-    //cout << "we in Can I Spell" << endl;
     while(BFS() == 1){
-        //cout << "nodes length " << nodes.size() << " " << endl;
-        Node *sink = nodes.back();
-        //cout << "BFS = 1" << endl;
-        cout << sink->id;
-        //cout << sink->type;
-        while(sink->type != src){
-            sink->backEdge->original = 0;
-            sink->backEdge->residual = 1;
-            sink->backEdge->reverse->original = 1;
-            sink->backEdge->reverse->residual = 0;
-            sink = sink->backEdge->from;
-            cout << " to " << sink->id;
+        Node *sink = nodes[nodes.size()-1];
+        if (!sink->backEdge) {
+            cerr << "Backedge for sink is null. Path trace may be invalid." << endl;
+        } else {
+            while (sink->type != src && sink->backEdge != nullptr) {
+
+                sink->backEdge->original = 0;
+                sink->backEdge->residual = 1;
+                if (sink->backEdge->reverse) {
+                    sink->backEdge->reverse->original = 1;
+                    sink->backEdge->reverse->residual = 0;
+                }
+                sink = sink->backEdge->from;
+            }
         }
-        cout <<  endl;
     }
+
+    Node *sink = nodes[nodes.size()-1];
+    int canSpell = 1;
+
+    for(int i = 0; i < sink->adj.size(); i++){
+        if(sink->adj[i]->original == 0){
+            canSpell = 0;
+        }
+    }
+
+    return canSpell;
 }
 
+//deletes the sink and word nodes and all edges that are connected to or from them
 void Graph::deleteHalfGraph() {
-    // Step 1: Delete all word nodes and their edges
+    for (Node *node : nodes) {
+        if (node->type == dice) {
+            for (auto it = node->adj.begin(); it != node->adj.end(); ) {
+                Edge *edge = *it;
+                if (edge->to->type == word) {
+                    delete edge;
+                    it = node->adj.erase(it);
+                } else {
+                    ++it; 
+                }
+            }
+        }
+    }
+
     for (int i = nodes.size() - 1; i >= minNodes; i--) {
         Node *node = nodes[i];
         
-        // Delete all edges attached to this word node
         for (Edge *edge : node->adj) {
             if (edge->reverse) {
-                edge->reverse->reverse = nullptr; // Unlink reverse edges
+                edge->reverse->reverse = nullptr;
             }
             delete edge;
         }
         
-        delete node; // Delete the word node
-        nodes.erase(nodes.begin() + i); // Remove node from the graph's node list
+        delete node; 
+        nodes.erase(nodes.begin() + i); 
     }
 
-    // Step 2: Remove edges in dice nodes that point to word nodes
-    for (Node *node : nodes) {
-        if (node->type == dice) {
-            // Filter out and delete edges pointing to word nodes
-            for (auto it = node->adj.begin(); it != node->adj.end(); ) {
-                Edge *edge = *it;
-                if (edge->to->type == word) {
-                    if (edge->reverse) {
-                        edge->reverse->reverse = nullptr; // Unlink reverse edge
-                        delete edge->reverse;
-                    }
-                    delete edge;
-                    it = node->adj.erase(it); // Erase edge from adj list and advance iterator
-                } else {
-                    ++it; // Advance only if no deletion
-                }
-            }
-        }
-    }
-
-    // Step 3: Reset the source node’s edges for the next search
     for (Edge *edge : nodes[0]->adj) {
         edge->original = 1;
         edge->residual = 0;
     }
 
-    // Step 4: Reset any visited and backEdge properties for the remaining nodes
     for (Node *node : nodes) {
         node->visited = 0;
         node->backEdge = nullptr;
     }
 }
 
+//deletes the whole graph, edges and nodes all of them
+void Graph::cleanupGraph() {
+    // Delete all edges and nodes in the graph
+    for (Node* node : nodes) {
+        for (Edge* edge : node->adj) {
+            // Only delete the edge if edge exists
+            if (edge) {
+                delete edge;
+            }
+        }
+        // Delete the node
+        delete node;
+    }
+
+    // Clear the whole nodes vector 
+    nodes.clear();
+}
+
 
 int main(int argc, char *argv[]) {
-
     if (argc < 3) {
         cerr << "Usage: " << argv[0] << " <diceFile> <wordFile>" << endl;
         return 1;
@@ -182,6 +222,7 @@ int main(int argc, char *argv[]) {
 	}
 
     string line;
+    //this should create a node for each dice so this is each line of the dice file
     while (getline(fin, line)) {
         nodeIndex++;
         Node *ourDice = new Node;
@@ -198,6 +239,7 @@ int main(int argc, char *argv[]) {
         fromSource->original = 1;
         fromSource->residual = 0;
 
+        //creating reverse edges 
         Edge *toSource = new Edge;
         toSource->from = ourDice;
         toSource->to = source;
@@ -205,15 +247,14 @@ int main(int argc, char *argv[]) {
         toSource->residual = 1;
 
         fromSource->reverse = toSource;
+        toSource->reverse = fromSource;
 
-        ourDice->backEdge = fromSource; //is this going the right direction? Do we need this yet? do we want this yet?
+        ourDice->backEdge = fromSource; 
         source->adj.push_back(fromSource);
         ourDice->adj.push_back(toSource);
         ourGraph.nodes.push_back(ourDice);
         ourGraph.minNodes++;
     }
-
-    
 
     fin.close();
     fin.open(wordFile);
@@ -221,47 +262,29 @@ int main(int argc, char *argv[]) {
     if(!fin.is_open()){
 		cerr << "Could not open: " << wordFile << endl;
 	}
-    
+    int wordCount = 0;
+    //this will create a node for each letter in each word that is given so 
+    //multiple nodes per line that is read
     while(getline(fin, line)){
-        //cout << "okay so the word file is open" << endl;
         nodeIndex = ourGraph.minNodes;
         Node *ourSink = new Node();
         ourSink->type = sink;
 
-        for(int i = 0; i < ourGraph.nodes.size(); i++){
-            cout << "Node " << ourGraph.nodes[i]->id << " ";
-            for(int j = 0; j < ourGraph.nodes[i]->letters.size(); j++){
-                if(ourGraph.nodes[i]->letters[j]){
-                    char letter = 'A' + j;
-                    cout << letter ;
-                }
-            }
-            cout << " Edges to " ;
-            for(int j = 0; j < ourGraph.nodes[i]->adj.size(); j++){
-                cout << ourGraph.nodes[i]->adj[j]->to->id << " "  ;
-            }
-            cout << endl ;
-        }
-        cout << endl << endl;
         for(int i = 0; i < line.length(); i++){
-            //cout << "creating nodes for letters" << endl;
             Node *aLetter = new Node();
-            //cout << "tell me its this stupud node index" << endl;
             aLetter->id = nodeIndex;
-           // cout << "hmm here?" << endl;
             aLetter->type = word;
             char letter = line[i];
             int diff = letter - 'A';
-            //cout << "or here? " << endl;
             aLetter->letters[diff]=true;
             nodeIndex++;
 
             for(int j = 1; j < ourGraph.minNodes; j++){
-                if(ourGraph.nodes[j]->letters[diff] == true){
-                    //cout << "creating edges for words" << endl;
+                if (ourGraph.nodes[j]->letters[diff] == true) {
+                    // Create edges for words
                     Edge *toWord = new Edge;
                     toWord->to = aLetter;
-                    toWord->from = ourGraph.nodes[j-1];
+                    toWord->from = ourGraph.nodes[j];
                     toWord->residual = 0;
                     toWord->original = 1;
 
@@ -272,11 +295,13 @@ int main(int argc, char *argv[]) {
                     toDice->residual = 1;
 
                     toWord->reverse = toDice;
+                    toDice->reverse = toWord;
+
+                    // Add edges to adjacency list
                     ourGraph.nodes[j]->adj.push_back(toWord);
                     aLetter->adj.push_back(toDice);
                 }
             }
-            //cout << "I am getting sick of this" << endl;
             Edge *toSink = new Edge;
             toSink->from = aLetter;
             toSink->to = ourSink;
@@ -286,10 +311,11 @@ int main(int argc, char *argv[]) {
             Edge *fromSink = new Edge;
             fromSink->from = ourSink;
             fromSink->to = aLetter;
-            fromSink->original = 1;
-            fromSink->residual = 0;
+            fromSink->original = 0;
+            fromSink->residual = 1;
 
             toSink->reverse = fromSink;
+            fromSink->reverse = toSink;
 
             aLetter->adj.push_back(toSink);
             ourSink->adj.push_back(fromSink);
@@ -299,44 +325,35 @@ int main(int argc, char *argv[]) {
         ourSink->id = nodeIndex;
         ourGraph.nodes.push_back(ourSink);
 
-        cout << " ok look here for the graphs after addition" << endl;
-        for(int i = 0; i < ourGraph.nodes.size(); i++){
-            if(ourGraph.nodes[i]->type == dice){
-                for (int j = 0; j < ourGraph.nodes[i]->adj.size(); j++) {
-                    cout << "edge type " << ourGraph.nodes[i]->adj[j]->to->id << endl;
-                    if(ourGraph.nodes[i]->adj[j]->to->type == word){
-                        cout << "edge type: " << ourGraph.nodes[i]->adj[j]->to->type << endl;
-                    // delete nodes[i]->adj[j];
-                    }
-                }   
-                //nodes[i]->adj.clear();
-            }
+        if(wordCount > 0){
+            cout << endl;
         }
-
-        //Now I am gonna print out the graph to make sure it looks right
-
-        for(int i = 0; i < ourGraph.nodes.size(); i++){
-            cout << "Node " << ourGraph.nodes[i]->id << " ";
-            for(int j = 0; j < ourGraph.nodes[i]->letters.size(); j++){
-                if(ourGraph.nodes[i]->letters[j]){
-                    char letter = 'A' + j;
-                    cout << letter ;
+        wordCount++;
+        //if the word can be spelled printing out the order of the dice 
+        //that is used for each letter in the words
+        if(ourGraph.canISpell() == 1){
+            for(int i = ourGraph.minNodes; i < ourGraph.nodes.size()-1; i++){
+                if(i > ourGraph.minNodes){
+                        cout << ",";
+                }
+                for(int j = 0; j < ourGraph.nodes[i]->adj.size(); j++){
+                    if(ourGraph.nodes[i]->adj[j]->to->type == dice && ourGraph.nodes[i]->adj[j]->original == 1){
+                        cout << (ourGraph.nodes[i]->adj[j]->to->id - 1);
+                    }
                 }
             }
-            cout << " Edges to " ;
-            for(int j = 0; j < ourGraph.nodes[i]->adj.size(); j++){
-                cout << ourGraph.nodes[i]->adj[j]->to->id << " "  ;
-            }
-            cout << endl ;
+            cout << ": " << line;
+        }else{
+            cout << "Cannot spell "  << line;
         }
-        cout << endl << endl;
 
-        //This is where we should do the BFS and path finding
-        //cout << " do we even get here? " << endl;
-        ourGraph.canISpell();    
+        //after each word has been determined we will delete the part 
+        //of the graph that corresponds to that word
         ourGraph.deleteHalfGraph();
 
     }
 
+    //delete whole graph no memory leaks please
+    ourGraph.cleanupGraph();
     return 0;
 }
